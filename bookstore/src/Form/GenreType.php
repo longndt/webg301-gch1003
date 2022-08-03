67f1e8b61b407581003cf105e64a7d8f9dc7043f
longndt <longndt226@gmail.com>
2022-08-03 15:12:52 +0700

    bookstore

--- bookstore/src/Form/GenreType.php
+++ bookstore/src/Form/GenreType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Genre;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,23 @@ class GenreType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('image')
+            ->add('name', TextType::class,
+            [
+                'required' => true, //optional (Not Null)
+                'label' => 'Genre name',
+                'attr' => [
+                    'minlength' => 3,
+                    'maxlength' => 30
+                ]
+            ])
+            ->add('image', TextType::class,
+            [
+                'label' => 'Genre image',
+                'attr' => [
+                    'maxlength' => 255
+                ]
+            ])
+            //->add('Save', SubmitType::class)
         ;
     }
 
