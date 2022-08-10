f7d6b160af861f6466b6873e8dd4f71812cad51c
longndt-fpt <longndt@fpt.edu.vn>
2022-08-10 15:42:51 +0700

    bookstore

--- bookstore/src/Entity/User.php
+++ bookstore/src/Entity/User.php
@@ -15,7 +15,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
-    private $id;
+   private $id; 
 
     #[ORM\Column(type: 'string', length: 180, unique: true)]
     private $username;
