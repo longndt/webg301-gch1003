f8cb354a3abd1fb804fa7d988152d999ec93f688
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-08-12 14:48:07 +0700

    bookstore

--- bookstore/src/DataFixtures/BookFixtures.php
+++ bookstore/src/DataFixtures/BookFixtures.php
@@ -10,9 +10,12 @@ class BookFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $titles = ["Java", "NET", "Python", "PHP", "ReactJS", "Spring"];
+
         for ($i=1; $i<=20; $i++) {
+            $key = array_rand($titles,1);
             $book = new Book;
-            $book->setTitle("Book $i")
+            $book->setTitle($titles[$key])
                  ->setQuantity(rand(10,100)) 
                  ->setPrice((float)(rand(100,1000))) 
                  ->setImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjwR_qwwP-PgM2uGa5crh8YvIhpV_yc7LNXA&usqp=CAU")
